d8aa74424f3b1ce349145039b9b66dc92da4109c
B1129036 <119582568+XieTangent@users.noreply.github.com>
2022-12-22 16:39:07 +0800

    Update main1.c

--- HW7/main1.c
+++ HW7/main1.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
 
+int main()
+{
+    int n=9;
+    for(int i=1;i<=n;i++){
+        for(int j=n;i<j;j--){
+            printf(" ");
+        }
+        for(int j=0;j<i;j++){
+            printf("%d ",i);
+        }
+    printf("\n");
+    }
+
+    return 0;
+}
