3308c42a183fab464016ff56696d9659cbda6d80
B1129036 <119582568+XieTangent@users.noreply.github.com>
2022-12-26 12:37:29 +0800

    Update main3.c

--- HW7/main3.c
+++ HW7/main3.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int x=1,y=1;x<10;y++){
+    printf("%d*%d=%d\t",x,y,x*y);
+    if(y==9){
+    x++;
+    y=0;
+    printf("\n");
+    }
+    }
+    return 0;
+}
 
