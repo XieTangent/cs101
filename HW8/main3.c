658d53d633ae0a6ce7af43af6b0ae56f92199ac0
B1129036 <119582568+XieTangent@users.noreply.github.com>
2022-12-31 01:56:44 +0800

    Update main3.c

--- HW8/main3.c
+++ HW8/main3.c
@@ -1,1 +1,45 @@
+#include <stdio.h>
 
+void get_binary(int i) {
+    int j=16,m=0;
+    while(j-1<i) {
+        j*=16;
+    }
+    j/=2;
+    while(i) {
+        if(j>i) {
+            printf("0");
+            j/=2;
+            m++;
+        }else if(j<i) {
+            printf("1");
+            i-=j;
+            j/=2;
+            m++;
+        }else{
+            printf("1");
+            m++;
+            break;
+        }
+        if(m==4) {
+            printf(" ");
+            m=0;
+        }
+    }
+    while(j!=1){
+        if(m==4){
+            printf(" ");
+            m=0;
+        }
+        printf("0");
+        m++;
+        j/=2;
+    }
+}
+
+int main() {
+    int i=255;
+    get_binary(i);
+
+    return 0;
+}
